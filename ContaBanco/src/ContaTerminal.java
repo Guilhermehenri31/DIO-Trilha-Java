81e5788c8dfa0a3ea8210e76483c73f6388cd9fe
Guilherme Henrique <deguilhermehenri@gmail.com>
2024-04-10 20:16:23 -0300

    Passo 2

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    static int saldo;
-    
+    static Scanner novo = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    System.out.println("Por favor, Digite o número da conta!");
+       int conta = novo.nextInt();
+    System.out.println("Digite o número da Agência: ");
+       int Agencia = novo.nextInt();
+    System.out.println("Digite nome do cliente: ");
+        String Nome_Cliente = novo.nextLine();
+    System.out.println("Digite o saldo: ");
+        Float Saldo = novo.nextFloat(); 
+
+    System.out.println("Olá " + Nome_Cliente + ", Obrigado por criar uma conta em nosso banco, sua agência é " + Agencia + ", Conta " + conta + "e seu saldo de " + Saldo + "Reais já está disponível em sua conta. Obrigado por fazer parte do nosso banco!");
+
+       
+
+    
     }
 }
